00167247b1888c08d0995d2bbe8c6b3e3d6e45e0
LinXi-AB <784410173@qq.com>
2024-11-10 10:31:49 +0800

    封装TCPServer为类

--- main.cpp
+++ main.cpp
@@ -1,109 +1,191 @@
-#include <stdio.h>  
-#include <winsock2.h>  
-#include <ws2tcpip.h>  // for inet_ntop
+#include <stdio.h>
+#include <winsock2.h>
+#include <ws2tcpip.h> 
+#include <string>
+#include <cstdint>
 
-#pragma comment(lib, "ws2_32.lib")  
+#pragma comment(lib, "ws2_32.lib")
+
+class TCPServer
+{
+public:
+	TCPServer();
+	~TCPServer();
+	bool initWinSock();
+	bool createSocket();
+	bool bindSocket(const char* ip, uint16_t port);
+	bool listenForConnections(int backlog = 5);
+	bool acceptConnection();
+	bool receiveData();
+	bool sendData(const std::string& data);
+	void closeConnection();
+
+private:
+	SOCKET listenSocket;
+	SOCKET clientSocket;
+	sockaddr_in serverAddr;
+	sockaddr_in clientAddr;
+	char clientIP[INET_ADDRSTRLEN];
+	std::string receivedData;
+};
+
+TCPServer::TCPServer() : listenSocket(INVALID_SOCKET), clientSocket(INVALID_SOCKET)
+{
+	memset(&serverAddr, 0, sizeof(serverAddr));
+	memset(&clientAddr, 0, sizeof(clientAddr));
+}
+
+TCPServer::~TCPServer()
+{
+	closeConnection();
+	WSACleanup();
+}
+
+bool TCPServer::initWinSock()
+{
+	WORD sockVersion = MAKEWORD(2, 2);
+	WSADATA wsaData;
+	if (WSAStartup(sockVersion, &wsaData) != 0)
+	{
+		printf("WSAStartup failed!\n");
+		return false;
+	}
+	return true;
+}
+
+bool TCPServer::createSocket()
+{
+	listenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (listenSocket == INVALID_SOCKET)
+	{
+		printf("Socket creation failed! Error code: %d\n", WSAGetLastError());
+		return false;
+	}
+	return true;
+}
+
+bool TCPServer::bindSocket(const char* ip, uint16_t port)
+{
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(port);
+	serverAddr.sin_addr.s_addr = (ip == nullptr) ? INADDR_ANY : inet_pton(AF_INET, ip, &serverAddr.sin_addr);
+
+	if (bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	{
+		printf("Bind failed! Error code: %d\n", WSAGetLastError());
+		return false;
+	}
+	return true;
+}
+
+bool TCPServer::listenForConnections(int backlog)
+{
+	if (listen(listenSocket, backlog) == SOCKET_ERROR)
+	{
+		printf("Listen failed! Error code: %d\n", WSAGetLastError());
+		return false;
+	}
+	return true;
+}
+
+bool TCPServer::acceptConnection()
+{
+	int clientAddrLen = sizeof(clientAddr);
+	clientSocket = accept(listenSocket, (SOCKADDR*)&clientAddr, &clientAddrLen);
+	if (clientSocket == INVALID_SOCKET)
+	{
+		printf("Accept failed! Error code: %d\n", WSAGetLastError());
+		return false;
+	}
+
+	if (inet_ntop(AF_INET, &clientAddr.sin_addr, clientIP, sizeof(clientIP)) == NULL)
+	{
+		printf("inet_ntop failed! Error code: %d\n", WSAGetLastError());
+		return false;
+	}
+
+	printf("Connection received from: %s\n", clientIP);
+	return true;
+}
+
+bool TCPServer::receiveData()
+{
+	char buffer[512];
+	int ret = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
+	if (ret > 0)
+	{
+		buffer[ret] = 0;
+		receivedData = buffer;
+		printf("Received from client: %s\n", receivedData.c_str());
+		return true;
+	}
+	else if (ret == 0)
+	{
+		printf("Connection closed by client.\n");
+		return false;
+	}
+	else
+	{
+		printf("Recv failed! Error code: %d\n", WSAGetLastError());
+		return false;
+	}
+}
+
+bool TCPServer::sendData(const std::string& data)
+{
+	int sendResult = send(clientSocket, data.c_str(), data.length(), 0);
+	if (sendResult == SOCKET_ERROR)
+	{
+		printf("Send failed! Error code: %d\n", WSAGetLastError());
+		return false;
+	}
+	return true;
+}
+
+void TCPServer::closeConnection()
+{
+	if (clientSocket != INVALID_SOCKET)
+	{
+		closesocket(clientSocket);
+		clientSocket = INVALID_SOCKET;
+	}
+	if (listenSocket != INVALID_SOCKET)
+	{
+		closesocket(listenSocket);
+		listenSocket = INVALID_SOCKET;
+	}
+}
 
 int main(int argc, char* argv[])
 {
-    // 初始化 Winsock
-    WORD sockVersion = MAKEWORD(2, 2);// WORD是一个十六位无符号整数，MAKEWORD将两个八位整数组合为16位，在此处用来表示socket的版本号2.2
-    WSADATA wsaData;// 按F12查看结构体定义，在WSAStartup中接受信息，如socket版本等
-    if (WSAStartup(sockVersion, &wsaData) != 0)// 初始化winsock使得可以使用网络功能，WSA是Windows Sockets API的缩写  
-    {
-        printf("WSAStartup failed!\n");
-        return 1;
-    }
+	TCPServer server;
 
-    // 创建套接字
-    SOCKET slisten = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);// IPv4地址族, TCP
-    if (slisten == INVALID_SOCKET)
-    {
-        printf("socket creation failed! Error code: %d\n", WSAGetLastError());
-        WSACleanup();
-        return 1;
-    }
+	if (!server.initWinSock())
+		return 1;
 
-    // 绑定 IP 和端口, bind
-    sockaddr_in sin;
-    sin.sin_family = AF_INET;// 地址族为IPv4
-    sin.sin_port = htons(8888);// 端口号为8888，0~65535，htons表示host to network short，主机字节序转化为网络字节序  
-    sin.sin_addr.S_un.S_addr = INADDR_ANY;// 设置IP地址，INADDR_ANY即0.0.0.0表示监听所有IP地址
-    if (bind(slisten, (LPSOCKADDR)&sin, sizeof(sin)) == SOCKET_ERROR)// 将套接字与地址和端口绑定
-    {
-        printf("bind failed! Error code: %d\n", WSAGetLastError());
-        closesocket(slisten);
-        WSACleanup();
-        return 1;
-    }
+	if (!server.createSocket())
+		return 1;
 
-    // 开始监听, listen
-    if (listen(slisten, 5) == SOCKET_ERROR)
-    {
-        printf("listen failed! Error code: %d\n", WSAGetLastError());
-        closesocket(slisten);
-        WSACleanup();
-        return 1;
-    }
+	if (!server.bindSocket(nullptr, 8888))// nullptr表示监听0.0.0.0
+		return 1;
 
-    // 循环接收数据
-    SOCKET sClient;
-    sockaddr_in remoteAddr;// 客户端地址信息
-    int nAddrlen = sizeof(remoteAddr);
-    char clientIP[INET_ADDRSTRLEN];  // 用来存储客户端的 IP 地址
-    while (true)
-    {
-        printf("Waiting for a connection...\n");
+	if (!server.listenForConnections())// 将socket转化为监听状态，使得服务端准备与客户端准备连接
+		return 1;
 
-        // 等待并接受客户端连接, accept
-        sClient = accept(slisten, (SOCKADDR*)&remoteAddr, &nAddrlen);// 阻塞，等待连接，返回绑定了地址的已连接的套接字
-        if (sClient == INVALID_SOCKET)
-        {
-            printf("accept failed! Error code: %d\n", WSAGetLastError());
-            continue; // 如果 accept 失败，继续等待新的连接
-        }
+	while (true)
+	{
+		printf("Waiting for a connection...\n");
 
-        // 使用 inet_ntop 来获取客户端的 IP 地址
-        if (inet_ntop(AF_INET, &remoteAddr.sin_addr, clientIP, sizeof(clientIP)) == NULL)
-        {
-            printf("inet_ntop failed! Error code: %d\n", WSAGetLastError());
-        }
-        else
-        {
-            printf("Connection received from: %s \n", clientIP);
-        }
+		if (!server.acceptConnection())
+			continue;
 
-        // 接收数据
-        char revData[255];// 存储从客户端接收到的数据
-        int ret = recv(sClient, revData, sizeof(revData) - 1, 0);
-        if (ret > 0)
-        {
-            revData[ret] = 0x00; // 确保字符串结尾
-            printf("Received from client: %s\n", revData);
-        }
-        else if (ret == 0)
-        {
-            printf("Connection closed by client.\n");
-        }
-        else
-        {
-            printf("recv failed! Error code: %d\n", WSAGetLastError());
-        }
+		if (!server.receiveData())// 返回false表示连接已失效
+			continue;
 
-        // 发送数据
-        const char* sendData = "Hello, TCP client!\n";
-        int sendResult = send(sClient, sendData, strlen(sendData), 0);
-        if (sendResult == SOCKET_ERROR)
-        {
-            printf("send failed! Error code: %d\n", WSAGetLastError());
-        }
+		const std::string sendData = "Hello, TCP client!\n";
+		if (!server.sendData(sendData))
+			continue;
+	}
 
-        // 关闭与客户端的连接
-        closesocket(sClient);
-    }
-
-    // 关闭监听 socket 并清理 Winsock
-    closesocket(slisten);
-    WSACleanup();// 清理winsock，winsock初始化对应
-    return 0;
+	return 0;
 }
